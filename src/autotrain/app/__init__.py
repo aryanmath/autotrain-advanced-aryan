0020010a7c386260e68d321a7e2d48f2b1e06627
aryanmath <164219390+aryanmath@users.noreply.github.com>
2025-06-12 08:20:03 +0530

    j

--- src/autotrain/app/__init__.py
+++ src/autotrain/app/__init__.py
@@ -1,10 +1,1 @@
-from flask import Flask
-from .routes.life_app_routes import life_app_bp
 
-def create_app():
-    app = Flask(__name__)
-    
-    # Register blueprints
-    app.register_blueprint(life_app_bp)
-    
-    return app
