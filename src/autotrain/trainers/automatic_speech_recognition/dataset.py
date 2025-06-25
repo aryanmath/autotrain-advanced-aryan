d2b81c64ae9d528d29d08a270974867fdb0b4a35
aryanmath <164219390+aryanmath@users.noreply.github.com>
2025-06-25 11:21:56 +0530

    hindi

--- src/autotrain/trainers/automatic_speech_recognition/dataset.py
+++ src/autotrain/trainers/automatic_speech_recognition/dataset.py
@@ -1,3 +1,8 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 import os
 import torch
 import librosa
